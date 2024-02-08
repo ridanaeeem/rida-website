c6f9f2de2d24657234fbf30e3f0192bdadcc182c
ridanaeeem <ridan@bu.edu>
2024-02-08 09:19:31 -0500

    mobile changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,17 @@
 import React from "react";
+import Image, { StaticImageData } from "next/image";
+import linkedIn from "/public/linkedinLogo.png";
+import gitHub from "/public/gitHubLogo.png";
 
 export default function Footer() {
 	return (
 		<footer className="flex text-xl fixed bottom-20">
 			{/* <div>Get in contact with me: </div> */}
 			<a className="invert pr-4" href="https://www.linkedin.com/in/rida-naeem-/" target="_blank">
-				<img
-					className="h-7 object-contain"
-					src="https://cdn-icons-png.flaticon.com/512/38/38669.png"
-					alt="LinkedIn logo"></img>
+				<Image className="object-contain" height={50} src={linkedIn} alt="LinkedIn logo" />
 			</a>
 			<a className="invert pr-4" href="https://github.com/ridanaeeem" target="_blank">
-				<img
-					className="h-7 object-contain"
-					src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-					alt="GitHub logo"></img>
+				<Image className="object-contain" height={50} src={gitHub} alt="GitHub logo" />
 			</a>
 			<a className="" href="mailto: ridan@bu.edu">
 				<div className="hover-underline-animation">
