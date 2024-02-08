e509179934235a3eedbfc996e25cebbf1080383a
ridanaeeem <ridan@bu.edu>
2024-02-08 09:54:22 -0500

    img fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image, { StaticImageData } from "next/image";
-import linkedIn from "/public/linkedinLogo.png";
+import linkedIn from "/public/linkedInLogo.png";
 import gitHub from "/public/gitHubLogo.png";
 
 export default function Footer() {
@@ -8,10 +8,18 @@ export default function Footer() {
 		<footer className="flex text-xl fixed bottom-20">
 			{/* <div>Get in contact with me: </div> */}
 			<a className="invert pr-4" href="https://www.linkedin.com/in/rida-naeem-/" target="_blank">
-				<Image className="object-contain" height={50} src={linkedIn} alt="LinkedIn logo" />
+				{/* <Image className="object-contain" height={50} src={linkedIn} alt="LinkedIn logo" /> */}
+				<img
+					className="h-7 object-contain"
+					src="https://cdn-icons-png.flaticon.com/512/38/38669.png"
+					alt="LinkedIn logo"></img>
 			</a>
 			<a className="invert pr-4" href="https://github.com/ridanaeeem" target="_blank">
-				<Image className="object-contain" height={50} src={gitHub} alt="GitHub logo" />
+				{/* <Image className="object-contain" height={50} src={gitHub} alt="GitHub logo" /> */}
+				<img
+					className="h-7 object-contain"
+					src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+					alt="GitHub logo"></img>
 			</a>
 			<a className="" href="mailto: ridan@bu.edu">
 				<div className="hover-underline-animation">
