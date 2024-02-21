6e1d3b0c7ca0b4dc5f9cd918d2e2e2946a0d23a9
ridanaeeem <ridan@bu.edu>
2024-02-20 20:20:58 -0500

    nav text size

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,9 @@ export default function Nav({ highlightNav }: { highlightNav: string }) {
 	const NavLink = ({ heading }: { heading: string }) => {
 		return (
 			<Link
-				className={`hover:text-white pr-5 md:p-0 ${highlightNav == heading ? "text-white" : "text-gray"}`}
+				className={`hover:text-white pr-5 md:p-0 text-xl ${
+					highlightNav == heading ? "text-white" : "text-gray"
+				}`}
 				href={`#${heading.toLowerCase()}`}>
 				{highlightNav == heading ? "——— " : null}
 				{heading}
